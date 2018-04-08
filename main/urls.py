255a4246354d86a4e9b651c6239eccdb22ffd934
jedrzej2j <piotr.jedrzejowski93@gmail.com>
2018-04-08 16:40:52 +0200

    test geta

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('monitordata', views.MonitorDataList.as_view(), name='monitordata'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
