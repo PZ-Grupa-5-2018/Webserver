2b8b1dd4cc7085ef0ab39f3a240cb00af68ead46
jedrzej2j <piotr.jedrzejowski93@gmail.com>
2018-05-13 12:58:56 +0200

    deleted temporary local monitor

--- main/urls.py
+++ main/urls.py
@@ -12,14 +12,6 @@ urlpatterns = [
     url(r'^monitors/(?P<monitor_id>[0-9]+)/download_monitor_data', views.downloadMonitorDetails),
     url(r'^monitors/(?P<monitor_id>[0-9]+)/hosts/(?P<host_id>[0-9]+)/$', views.hosts_detail),
     url(r'^monitors/(?P<monitor_id>[0-9]+)/hosts/(?P<host_id>[0-9]+)/metrics/(?P<metric_id>[0-9]+)/$', views.metrics_detail),
-
-
-    url(r'^hosts/$', views.HostList.as_view()),
-    url(r'^hosts/(?P<pk>[0-9]+)/$', views.HostDetail.as_view()),
-    url(r'^hosts/(?P<host_id>[0-9]+)/metrics/$', views.MetricList.as_view()),
-    url(r'^hosts/(?P<host_id>[0-9]+)/metrics/(?P<pk>[0-9]+)/$', views.MetricDetail.as_view()),
-    url(r'^hosts/(?P<host_id>[0-9]+)/metrics/(?P<metric_id>[0-9]+)/measurements/$', views.MeasurementList.as_view()),
-    url(r'^hosts/(?P<host_id>[0-9]+)/metrics/(?P<metric_id>[0-9]+)/measurements/(?P<measurement_id>[0-9]+)/$', views.MeasurementDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
