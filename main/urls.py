5798a59626df316980579c9c755d90bb3347658b
not4juu <Sebastian.Kaluzny@fis.agh.edu.pl>
2018-05-22 22:52:52 +0200

    Add refreshing charts by 5s

--- main/urls.py
+++ main/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     url(r'^monitors/(?P<monitor_id>[0-9]+)/download_monitor_data', views.downloadMonitorDetails),
     url(r'^monitors/(?P<monitor_id>[0-9]+)/get_last_measurements', views.getLastMeasurementsView),
     url(r'^monitors/(?P<monitor_id>[0-9]+)/hosts/(?P<host_id>[0-9]+)/$', views.hosts_detail),
+    url(r'^monitors/(?P<monitor_id>[0-9]+)/hosts/(?P<host_id>[0-9]+)/refresh_chart_measurements', views.refreshChartMeasurments),
     url(r'^monitors/(?P<monitor_id>[0-9]+)/hosts/(?P<host_id>[0-9]+)/metrics/(?P<metric_id>[0-9]+)/$', views.metrics_detail),
 
     url(r'^search/$', views.search),
---
+++ webserver/static/js/refreshChartMeasurments.js
@@ -0,0 +1,53 @@
+var interval_refresh_last_measurements = window.setInterval('refresh_chart_measruments()', 5000); // 10 seconds
+
+var drawChart =  function (dataChart) {
+    nv.addGraph(function() {
+      var chart = nv.models.lineChart()
+          .useInteractiveGuideline(true)
+      ;
+        chart.xAxis
+            .axisLabel('Time')
+            .tickFormat(function(d) {
+                 return d3.time.format('%X')(new Date(d*1000))
+            });
+        ;
+        chart.yAxis
+            .axisLabel('Usage')
+            .tickFormat(d3.format('.02f'))
+        ;
+
+        d3.select("#chart svg")
+            .datum(dataChart)
+            .transition().duration(1200)
+            .call(chart);
+
+      return chart;
+});
+};
+
+
+var refresh_chart_measruments = function() {
+    $.ajax({
+        url: "refresh_chart_measurements/",
+        success: function( chart_data_measurments ) {
+            drawChart(chart_data_measurments);
+        },
+        error: function(ts,resp,error) {
+            console.log("not work - response: "+resp+" - error: "+error);
+        }
+    });
+};
+
+refresh_chart_measruments();
+
+function startRefresh() {
+    interval_refresh_last_measurements = window.setInterval('refresh_chart_measruments()', 5000);
+    refresh_chart_measruments();
+    document.getElementById("startRefresh").disabled = true;
+    document.getElementById("stopRefresh").disabled = false;
+}
+function stopRefresh(){
+    clearInterval(interval_refresh_last_measurements);
+    document.getElementById("startRefresh").disabled = false;
+    document.getElementById("stopRefresh").disabled = true;
+}
