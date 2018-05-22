94693cc2fe89d5ff26e0912fdfea5ad47efa14e2
jedrzej2j <piotr.jedrzejowski93@gmail.com>
2018-05-22 13:50:26 +0200

    test ajax1

--- webserver/static/js/js.js
+++ webserver/static/js/js.js
@@ -1,2 +1,30 @@
 $(document).ready(function(){
+    $.ajax({
+        url: "https://pz-monitor-2.herokuapp.com/hosts/1/",
+        success: function( data ) {
+            alert("work");
+        },
+        error: function(ts) {
+            alert("dont work");
+        }
+    }).then(function(data) {
+       $('#test_id').append(data.id);
+       $('#test_name').append(data.name);
+    });
 });
+
+/*
+var proxy_url_tmp = "https://pz-webserver.herokuapp.com/";
+
+$.ajax({
+   type: "GET",
+   dataType: "json",
+   url: proxy_url_tmp + "https://pz-monitor-2.herokuapp.com/hosts/1/",
+   success: function(data){
+   alert("work");
+   },
+   error: function(a,b,c){
+   alert("not work");
+   }
+});
+*/
