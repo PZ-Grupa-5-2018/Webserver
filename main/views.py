ec23a6600b04ec43d25175488457852603fb8911
Kamil <kamilbajdo@gmail.com>
2018-05-23 19:07:56 +0200

    Zapomnialem skasowac printow :D

--- main/views.py
+++ main/views.py
@@ -241,8 +241,6 @@ def search_host(request):
             if do_add:
                 all_metric_data.extend(metric_data)
 
-        print(all_metric_data)
-
         context = {
             'data': all_metric_data,
         }
